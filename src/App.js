c7c493c475744ccd46533f53edb67374d24d489e
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-05-04 13:43:41 +0600

    initial commit | starting state of the project | added react redux and redux toolkit packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Cart from "./Components/Cart/Cart";
+import Layout from "./Components/Layout/Layout";
+import Products from "./Components/Shop/Products";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
