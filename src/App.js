880b3ab2477661989e6d5ae6e2c1d6004ddc42c6
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-05-05 14:25:59 +0600

    Updating the App.js file to get the cart state from the store and send a PUT request to the firebase database to update any changes that happen to the cart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./Components/Cart/Cart";
@@ -6,6 +7,12 @@ import Products from "./Components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+
+  useEffect(() => {
+    fetch("https://react-http-7f732-default-rtdb.firebaseio.com/cart.json", { method: "PUT", body: JSON.stringify(cart) });
+  }, [cart]);
 
   return (
     <Layout>
